615fac46c900e02d080dc5894b8bfcd2fe7dcbab
Maxi Centeno <contactmaxi531@gmail.com>
2024-02-03 01:21:59 -0300

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css'
+
 export function App() {
     return (
-        <article style={{ display: 'flex', alignItems:'center', color:'#fff'}}>
-            <header>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
                 <img alt="Avatar" src="https://us.123rf.com/450wm/yupiramos/yupiramos1609/yupiramos160912719/62358443-avatar-hombre-sonriente-de-dibujos-animados-persona-de-sexo-masculino-de-usuario-ilustraci%C3%B3n.jpg" />
                 <div>
                     <strong>Maxi Centeno</strong>
